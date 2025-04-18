977b80e71d99c00f3befc3e879a4529768f09811
lyhottag13 <leonardo@promoevt.com>
2025-04-17 20:05:56 -0700

    Updated Sounds

--- js/Main.js
+++ js/Main.js
@@ -1,27 +1,22 @@
 import { SoundManager } from "./SoundManager.js";
-const initialString = "There's nobody here..."
 const textBox = document.getElementById("text");
 const button = document.getElementById("closedSign");
 const player = new SoundManager();
 let isTyping = true;
 
-const AUDIO = ["wind", "generic1"];
+const AUDIO = ["wind", "generic1", "generic2"];
 
 AUDIO.forEach((name) => {
     player.load(name);
 });
 
 window.onload = async () => {
-    await sleep(4000);
-    for (let i = 0; i < initialString.length; i++) {
-        textBox.textContent += initialString.charAt(i);
-        player.play("generic1");
-        await sleep(35);
-    }
+    await sleep(2000);
     isTyping = false;
+    displayText("There's nobody here...");
 };
 button.addEventListener("pointerdown", () => {
-    displayText("The sign appears to be hastily painted with a sharpie.");
+    displayText("The sign appears to be hastily painted on with a sharpie.");
 });
 async function displayText(text) {
     if (!isTyping) {
@@ -29,7 +24,7 @@ async function displayText(text) {
         isTyping = true;
         for (let i = 0; i < text.length; i++) {
             textBox.textContent += text.charAt(i);
-            player.play("generic1");
+            player.play("generic2");
             await sleep(35);
         }
         isTyping = false;
