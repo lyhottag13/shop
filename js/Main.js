597a71751e48bc40187386913b81a19c08a23092
lyhottag13 <leonardo@promoevt.com>
2025-04-17 22:17:07 -0700

    Updated Style and Icon

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,7 @@
 import { SoundManager } from "./SoundManager.js";
 const textBox = document.getElementById("text");
 const closedSign = document.getElementById("closedSign");
+const lightSwitch = document.getElementById("lightSwitch");
 const player = new SoundManager();
 let isTyping = true;
 
@@ -11,25 +12,41 @@ AUDIO.forEach((name) => {
 });
 
 window.onload = async () => {
-    await sleep(2000);
-    isTyping = false;
-    displayText("There's nobody here...");
+    initialize();
 };
+let signClicks = {number: 0};
 closedSign.addEventListener("pointerdown", () => {
-    displayText("The sign appears to be hastily painted on with a sharpie.");
+    if (signClicks.number === 3) {
+        display("You observe that this sign has nothing left to observe.");
+    } else {
+        display("The sign appears to be hastily painted on with a sharpie.", signClicks);
+    }
+});
+lightSwitch.addEventListener("pointerdown", () => {
+    display("This string has seen better days.");
 });
 
-
+async function initialize() {
+    await sleep(2000);
+    isTyping = false;
+    display("There's nobody here...");
+}
 async function displayText(text) {
+    textBox.textContent = "* ";
+    isTyping = true;
+    for (let i = 0; i < text.length; i++) {
+        textBox.textContent += text.charAt(i);
+        player.play("generic2");
+        await sleep(35);
+    }
+    isTyping = false;
+}
+async function display(text, counter) {
     if (!isTyping) {
-        textBox.textContent = "* "; 
-        isTyping = true;
-        for (let i = 0; i < text.length; i++) {
-            textBox.textContent += text.charAt(i);
-            player.play("generic2");
-            await sleep(35);
+        displayText(text);
+        if (counter !== undefined) {
+            counter.number++;
         }
-        isTyping = false;
     }
 }
 function sleep(ms) {
