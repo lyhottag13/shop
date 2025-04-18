7b93ba327cf8bd2423423bb83d7073d3e32c338d
lyhottag13 <leonardo@promoevt.com>
2025-04-18 00:37:44 -0700

    Added Animations

---
+++ js/Animator.js
@@ -0,0 +1,22 @@
+export class Animator {
+    constructor() {
+        this.FRAMERATE = 12;
+        this.FRAMES_PER_ROW = 1;
+        this.FRAME_WIDTH = 500;
+        this.ROW_LENGTH = this.FRAMES_PER_ROW * this.FRAME_WIDTH;
+        this.viewX = 0
+        this.x = 0;
+        this.animation = document.getElementById("animation");
+    }
+    animate() {
+        setInterval(() => {
+            this.animation.style.backgroundPosition = `-${this.viewX}px 0`;
+            this.x += this.FRAME_WIDTH;
+            this.viewX = this.x % this.ROW_LENGTH;
+        }, 1000 / this.FRAMERATE);
+    }
+    setFrames(frames) {
+        this.FRAMES_PER_ROW = frames;
+        this.ROW_LENGTH = this.FRAMES_PER_ROW * this.FRAME_WIDTH
+    }
+}
--- js/Main.js
+++ js/Main.js
@@ -1,11 +1,22 @@
 import { SoundManager } from "./SoundManager.js";
+import { Animator } from "./Animator.js";
 const textBox = document.getElementById("text");
 const closedSign = document.getElementById("closedSign");
 const lightSwitch = document.getElementById("lightSwitch");
 const player = new SoundManager();
+const animator = new Animator();
 let isTyping = true;
 
 const AUDIO = ["wind", "generic1", "generic2"];
+const IMAGES = ["Idle", "Closed"];
+
+const images = {};
+
+IMAGES.forEach((name) => {
+    const image = new Image();
+    image.src = `resources/images/${name}.png`;
+    images[name] = image;
+});
 
 AUDIO.forEach((name) => {
     player.load(name);
@@ -13,41 +24,60 @@ AUDIO.forEach((name) => {
 
 window.onload = async () => {
     initialize();
+    animator.animate();
 };
 let signClicks = {number: 0};
 closedSign.addEventListener("pointerdown", () => {
     if (signClicks.number === 3) {
-        display("You observe that this sign has nothing left to observe.");
+        display("* You observe that this sign has nothing left to observe.");
+        changeAnimation("Idle", 17);
+        closedSign.remove();
+        createButton("ali", "40%", "30%", "40%", "30%", ali);
     } else {
-        display("The sign appears to be hastily painted on with a sharpie.", signClicks);
+        display("* The sign appears to be hastily painted on with a sharpie.", signClicks);
     }
 });
 lightSwitch.addEventListener("pointerdown", () => {
     display("This string has seen better days.");
 });
-
+function ali() {
+    display("Hello!");
+}
 async function initialize() {
     await sleep(2000);
     isTyping = false;
-    display("There's nobody here...");
-}
-async function displayText(text) {
-    textBox.textContent = "* ";
-    isTyping = true;
-    for (let i = 0; i < text.length; i++) {
-        textBox.textContent += text.charAt(i);
-        player.play("generic2");
-        await sleep(35);
-    }
-    isTyping = false;
+    display("* There's nobody here...");
 }
 async function display(text, counter) {
     if (!isTyping) {
-        displayText(text);
+        textBox.textContent = "";
+        isTyping = true;
+        for (let i = 0; i < text.length; i++) {
+            textBox.textContent += text.charAt(i);
+            player.play("generic2");
+            await sleep(35);
+        }
+        isTyping = false;
         if (counter !== undefined) {
             counter.number++;
         }
     }
+}
+function changeAnimation(fileName, frames) {
+    document.getElementById("animation").style.backgroundSize = `${frames * 500}px 500px`;
+    document.getElementById("animation").style.backgroundImage = `url("resources/images/${fileName}.png")`;
+    animator.setFrames(17);
+}
+function createButton(id, top, left, width, height, functionName) {
+    const button = document.createElement("button");
+    button.id = id;
+    button.classList.add("interactable");
+    button.style.top = top;
+    button.style.left = left;
+    button.style.width = width;
+    button.style.height = height;
+    document.getElementById("counter").appendChild(button);
+    button.addEventListener("pointerdown", functionName);
 }
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
