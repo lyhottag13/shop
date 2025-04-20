44f076a6fa7b58781af1478365c1fcf43e091967
lyhottag13 <leonardo@promoevt.com>
2025-04-20 00:01:12 -0700

    Animation Overhaul

--- js/Animator.js
+++ js/Animator.js
@@ -7,12 +7,11 @@ export class Animator {
         this.animation = document.getElementById(source);
     }
     setAnimation(imageSource, frames, framerate, end) {
-        document.documentElement.style.setProperty("--animation-image", `url(${imageSource.src})`);
-        document.documentElement.style.setProperty("--background-width", this.FRAME_WIDTH * frames);
-        document.documentElement.style.setProperty("--background-size", `${this.FRAME_WIDTH * frames}px, ${this.FRAME_WIDTH}px`);
-        document.documentElement.style.setProperty("--frames", (frames - 1));
-        document.documentElement.style.setProperty("--animation-length", `${frames / framerate}s`);
+        this.animation.style.setProperty("--animation-image", `url(${imageSource.src})`);
+        this.animation.style.setProperty("--background-width", this.FRAME_WIDTH * frames);
+        this.animation.style.setProperty("--background-size", `${this.FRAME_WIDTH * frames}px, ${this.FRAME_WIDTH}px`);
+        this.animation.style.setProperty("--frames", (frames - 1));
+        this.animation.style.setProperty("--animation-length", `${frames / framerate}s`);
         this.animation.style.animation = `anim var(--animation-length) steps(var(--frames)) ${end}`;
-        console.log()
     }
 }
