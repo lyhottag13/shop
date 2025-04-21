b8bdc5c506020cb60bf606ddd90a6f7db46fa4d8
lyhottag13 <leonardo@promoevt.com>
2025-04-20 22:57:53 -0700

    Updated Sequencing

--- js/Main.js
+++ js/Main.js
@@ -4,21 +4,20 @@ const textBox = document.getElementById("text");
 const body = document.body;
 const MOBILE = (window.innerWidth <= 600) ? true : false;
 const FRAME_WIDTH = (window.innerWidth <= 600) ? 300 : 500;
-const player = new SoundManager();
-const counterAnimator = new Animator(FRAME_WIDTH, "animation");
 const switchAnimator = new Animator(FRAME_WIDTH, "switchAnimation");
+const counterAnimator = new Animator(FRAME_WIDTH, "animation");
+const player = new SoundManager();
 let isTyping = true;
 let isLightOn = false;
 let isInteractionAllowed = true;
 
-const AUDIO = ["wind", "shop", "generic1", "generic2"];
-const IMAGES = ["Closed", "ClosedLights", "Idle", "OpeningBusiness", "Switch", "SwitchPull"];
+const AUDIO = ["wind", "shop", "generic1", "generic2", "lightclick", "lightappear"];
+const IMAGES = ["Closed", "ClosedLights", "Idle", "OpeningBusiness", "Switch", "SwitchPull", "Background"];
 
 const images = {};
 const eventHandlers = {
     switchLightsEvent,
     closedSignEvent,
-    signGoneEvent,
     aliEvent,
 };
 IMAGES.forEach((name) => {
@@ -37,7 +36,12 @@ window.onload = () => {
         initialize();
     }, { once: true });
 };
-
+async function initialize() {
+    player.playBackgroundMusic("wind");
+    isTyping = false;
+    await displayText("* There's nobody here...");
+    createButton("closedSign", (MOBILE) ? "65%" : "62%", "5%", "90%", "20%", () => callEvent("closedSignEvent"), "counter");
+}
 let signClicks = { number: 0 };
 async function callEvent(eventName) {
     if (isInteractionAllowed) {
@@ -49,59 +53,56 @@ async function callEvent(eventName) {
 async function switchLightsEvent() {
     if (!isTyping) {
         if (!isLightOn) {
-            displayText("* ?");
-            const totalFrames = 28;
+            const lightFrame = 13;
             const fps = 25;
-            switchAnimator.setAnimation(images["SwitchPull"], totalFrames, fps, "forwards");
-            await sleep(13 / fps * 1000);
+            displayText("* ?");
+            switchAnimator.setAnimation(images["SwitchPull"], 28, fps, "forwards");
+            await sleep(lightFrame / fps * 1000);
+            // Lights activate here.
+            player.play("lightclick");
+            player.stopBackgroundMusic();
             counterAnimator.setAnimation(images["ClosedLights"], 1, 1, "forwards");
-            body.style.backgroundImage = "url('resources/images/Background.webp')";
+            body.style.backgroundImage = `url("${images["Background"].src}")`;
             body.style.backgroundColor = "rgb(179, 115, 10)";
             isLightOn = true;
-            player.stopBackgroundMusic();
+            document.getElementById("closedSign").remove();
             await sleep(1500);
+            await displayText("* The switch is now on.");
+            await sleep(2000);
+            await displayText("* You hear shuffling behind the counter.");
+            await sleep(2000);
+            counterAnimator.setAnimation(images["OpeningBusiness"], 21, 23, "forwards");
+            await sleep(21 / 23 * 1000 + 1500);
+            await displayText("* You are filled with CURIOSITY.#* You decide to peek over the counter...");
+            await sleep(1800);
+            player.playBackgroundMusic("shop");
+            counterAnimator.setAnimation(images["Idle"], 17, 12, "infinite");
+            createButton("ali", "40%", "10%", "80%", "60%", () => callEvent("aliEvent"), "counter");
+        } else {
+            await displayText("* The switch is now on.");
         }
-        await displayText("* The switch is now on.");
     }
     return;
 }
 async function closedSignEvent() {
-    if (signClicks.number > 1 && isLightOn === true) {
-        displayText("* ?");
-        counterAnimator.setAnimation(images["OpeningBusiness"], 21, 23, "forwards");
-        document.getElementById("closedSign").remove();
-        await sleep(21 / 23 * 1000 + 1000);
-        createButton("signGone", "40%", "30%", "40%", "30%", () => callEvent("signGoneEvent"), "counter");
-        await displayText("* The sign has mysteriously vanished.");
-    } else if (signClicks.number > 1) {
+    if (signClicks.number > 2) {
+        await displayText("* You observe that this sign has nothing left to observe.");
+    } else if (signClicks.number === 2) {
         await displayText("* You observe that this sign has nothing left to observe.", signClicks);
-    } else if (signClicks.number === 1) {
-        document.getElementById("switch").style.opacity = 1;
+        await sleep(1000);
+        player.play("lightappear");
         createButton("lightSwitch", "0", "80%", "7%", (MOBILE) ? "60%" : "94%", () => callEvent("switchLightsEvent"), "switch");
-        await displayText("* You observe that this sign has nothing left to observe.", signClicks);
+        document.getElementById("switch").style.opacity = 1;
+    } else if (signClicks.number === 1) {
+        await displayText("* The sign's penmanship is impeccable.", signClicks);
     } else {
-        await displayText("* The sign appears to be hastily painted on with a sharpie.", signClicks);
+        await displayText("* The sign appears to be hastily drawn on with a sharpie.", signClicks);
     }
     return;
 }
-async function signGoneEvent() {
-    await displayText("* You peer over the counter...");
-    await sleep(1800);
-    player.playBackgroundMusic("shop");
-    counterAnimator.setAnimation(images["Idle"], 17, 12, "infinite");
-    document.getElementById("signGone").remove();
-    createButton("ali", "40%", "30%", "40%", "30%", () => callEvent("aliEvent"), "counter");
-    return;
-}
 async function aliEvent() {
-    await displayText("Howdy, I'm Ali!|Welcome to my shop!#I'm still setting up, but feel free to look around!");
+    await displayText("Howdy, I'm Ali!|Welcome to my shop!#I'm still setting up, but feel free to stick around!");
     return;
-}
-async function initialize() {
-    player.playBackgroundMusic("wind");
-    isTyping = false;
-    await displayText("* There's nobody here...");
-    createButton("closedSign", (MOBILE) ? "65%" : "62%", "32%", "36%", "9%", () => callEvent("closedSignEvent"), "counter");
 }
 async function displayText(text, counter) {
     if (!isTyping) {
