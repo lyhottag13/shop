5935fc8cd8ecfe2c752aa42fc859cb653bb804da
lyhottag13 <leonardo@promoevt.com>
2025-04-26 22:40:14 -0700

    Setting Up ShopImages JSON

--- js/Main.js
+++ js/Main.js
@@ -8,16 +8,21 @@ const switchAnimator = new Animator(FRAME_WIDTH, "switchAnimation");
 const counterAnimator = new Animator(FRAME_WIDTH, "animation");
 const doorAnimator = new Animator(FRAME_WIDTH, "doorAnimation");
 
+const screens = document.querySelectorAll(".screen");
 
 const shopContainer = document.getElementById("shopContainer");
 const shopTab = document.getElementById("shopTab");
 const shopMenu = document.getElementById("shopMenu");
 
+const counter = document.getElementById("counter");
+const dialogueBox = document.getElementById("textDiv");
+
+
 const player = new SoundManager();
 let isTyping = false;
 let isInteractionAllowed = true;
 let isInitialized = false;
-let currentScreenNumber = 0;
+let currentScreenIndex = 0;
 let isMenuShowing = false;
 const AUDIO = ["wind", "shop", "generic1", "generic2", "lightclick", "lightappear", "doorcreak", "explosion"];
 const IMAGES = ["Closed", "ClosedLights", "Idle", "OpeningBusiness", "Switch", "SwitchPull", "Background", "SwitchPull1", "SwitchPull2", "Face", "Door"];
@@ -28,34 +33,39 @@ let skip = false;
 
 
 const images = {};
+const shopImages = {};
 const eventHandlers = {
     switchLightsEvent,
     closedSignEvent,
     aliEvent,
     doorEvent,
-    shopClickEvent,
 };
 player.construct();
-AUDIO.forEach(name => {
-    player.load(name);
-});
 
-IMAGES.forEach((name) => {
-    const image = new Image();
-    image.src = `resources/images/${name}.webp`;
-    images[name] = image;
-});
 
 const openDialogues = new Map();
 
-let response;
 let dialogue;
 
 let isShowingInitialText;
 window.onload = async () => {
-    response = await fetch("js/Dialogue.json");
-    dialogue = await response.json();
-    setScreen(0, 0);
+    dialogue = await (await fetch("js/Dialogue.json")).json();
+    const image = await (await (fetch("Resources.json"))).json();
+    AUDIO.forEach(name => {
+        player.load(name);
+    });
+    image["images"].forEach((name) => {
+        const image = new Image();
+        image.src = `resources/images/${name}.webp`;
+        images[name] = image;
+    });
+    image["shopImages"].forEach((name, index) => {
+        const image = new Image();
+        image.src = `resources/images/${name}.webp`;
+        shopImages[index] = image;
+    });
+
+    setScreen({ nextScreenIndex: 0, betweenScreenTime: 0 });
     document.addEventListener("click", async () => {
         if (!isInitialized) {
             initialize();
@@ -73,7 +83,7 @@ window.onload = async () => {
     }
 };
 
-let currentShopItem = 0;
+let currentShopIndex = 0;
 const arrayOfItems = document.querySelectorAll(".shopItem");
 
 // This starts the game when the user clicks/taps.
@@ -86,13 +96,18 @@ async function initialize() {
     doorAnimator.setAnimation(images["Door"], 17, 0, "forwards");
     switchAnimator.setAnimation(images["Switch"], 1, 0, "forwards");
     counterAnimator.setAnimation(images["Closed"], 1, 0, "forwards");
-    document.getElementById("screen1").style.display = "flex";
+    screens[1].style.display = "flex";
 
     // This will listen for key presses, if I press c, I initiate "skip" mode.
     document.addEventListener("keydown", event => keyHandler(event));
 
     // If the screen is currently showing the initial text, then it'll wait a little longer.
-    setScreen(1, isShowingInitialText ? 1500 : 0, isShowingInitialText ? "1.5s" : "0s", "3s");
+    setScreen({
+        nextScreenIndex: 1,
+        betweenScreenTime: isShowingInitialText ? 1500 : 0,
+        fadeOut: isShowingInitialText ? 1500 : 0,
+        fadeIn: 3000
+    });
     player.playBackgroundMusic("wind");
     await sleep(2000);
     // TEMP Testing Shop Menu
@@ -170,7 +185,7 @@ async function doorEvent() {
     // The door will zoom in here.
     document.getElementById("screen1").style.transform = "scale(2)";
     // The website swaps to the other screen now.
-    setScreen(2, 3000);
+    setScreen({ nextScreenIndex: 2, betweenScreenTime: 3000 });
     player.setBackgroundVolume(-1, 0.3, 4);
     await sleep(5000);
     createButton("closedSign", MOBILE ? "65%" : "62%", "5%", "90%", "20%", () => callEvent("closedSignEvent"), "counter");
@@ -229,6 +244,8 @@ async function switchLightsEvent() {
 }
 async function aliEvent() {
     if (!openDialogues.get("ali")) {
+        // This triggers if this is the first time we've talked on Ali.
+        const descriptionParts = document.querySelectorAll("#description span");
         await newText({ dialogueName: "ali" });
         shopTab.addEventListener("pointerdown", toggleMenu);
         shopContainer.style.visibility = "visible";
@@ -236,53 +253,49 @@ async function aliEvent() {
         arrayOfItems.forEach((item, itemIndex) => {
             // This shows the 0th shop item by default.
             if (itemIndex === 0) {
-                document.querySelectorAll("#description span")[0].textContent = `${dialogue["itemDescription"][0]}`;
-                document.querySelectorAll("#description span")[1].textContent = `${dialogue["itemDescription"][1]}`;
+                descriptionParts[0].textContent = dialogue["itemHeader"][0];
+                descriptionParts[1].textContent = dialogue["itemDescription"][0];
                 item.style.visibility = "visible";
             } else {
                 item.style.visibility = "hidden";
-                item.style.transform = `translate${MOBILE ? "X(300" : "Y(300"}px)`;
+                item.style.transform = `translate${MOBILE ? "X" : "Y"}(300px)`;
             }
             // If any shop item is clicked, then it will trigger the dialogue.
             item.addEventListener("pointerdown", async () => {
                 toggleMenu();
                 shopContainer.style.opacity = 0;
                 shopTab.removeEventListener("pointerdown", toggleMenu);
-                await newText({ dialogueName: "shopItems", index: itemIndex > 3 ? MOBILE ? 1 : 0 : itemIndex + 2 });
+                await newText({ dialogueName: "itemDialogue", index: itemIndex });
                 shopContainer.style.opacity = 1;
                 shopTab.addEventListener("pointerdown", toggleMenu);
             });
         });
         document.querySelectorAll(".arrow").forEach((arrow, arrowIndex) => {
+            const arrowImage = arrow.querySelector("img");
             if (arrowIndex === 0) {
-                arrow.querySelector("img").style.transform = `rotate(${MOBILE ? "0" : "90deg"})`;
+                arrowImage.style.transform = `rotate(${MOBILE ? "0" : "90deg"})`;
             } else {
-                arrow.querySelector("img").style.transform = `rotate(${MOBILE ? "180deg" : "270deg"})`
+                arrowImage.style.transform = `rotate(${MOBILE ? "180deg" : "270deg"})`
             }
+            const direction = arrowIndex === 0 ? "" : "-";
+            const displacement = `${direction}200px`;
             arrow.addEventListener("pointerdown", () => {
-                // arrayOfItems[currentShopItem].style.visibility = "hidden";
-                // This will increment the shop item currently showing.
-                // This clamps the index so it doesn't go out of range.
-                // currentShopItem = Math.max(0, Math.min(arrayOfItems.length - 1, currentShopItem));
-                if (arrowIndex === 0 && currentShopItem !== 0) {
-                    arrayOfItems[currentShopItem].style.transform = `translate${MOBILE ? "X(200" : "Y(200"}px)`;
-                    arrayOfItems[currentShopItem].style.filter = "opacity(0)";
-                    currentShopItem--;
-                    arrayOfItems[currentShopItem].style.transform = `translate${MOBILE ? "X(0" : "Y(0"})`;
-                    arrayOfItems[currentShopItem].style.filter = "opacity(1)";
-                } else if (arrowIndex === 1 && currentShopItem !== arrayOfItems.length - 1) {
-                    arrayOfItems[currentShopItem].style.transform = `translate${MOBILE ? "X(-200" : "Y(-200"}px)`;
-                    arrayOfItems[currentShopItem].style.filter = "opacity(0)";
-                    currentShopItem++;
-                    arrayOfItems[currentShopItem].style.transform = `translate${MOBILE ? "X(0" : "Y(0"})`;
-                    arrayOfItems[currentShopItem].style.filter = "opacity(1)";
+                const isValidMove = ((arrowIndex === 0 && currentShopIndex !== 0) || (arrowIndex === 1 && currentShopIndex !== arrayOfItems.length - 1));
+                if (isValidMove) {
+                    arrayOfItems[currentShopIndex].style.transform = `translate${MOBILE ? "X" : "Y"}(${displacement})`;
+                    arrayOfItems[currentShopIndex].style.filter = "opacity(0)";
+                    currentShopIndex += arrowIndex === 0 ? -1 : 1;
                 }
-                arrayOfItems[currentShopItem].style.visibility = "visible";
-                document.querySelectorAll("#description span")[0].textContent = dialogue["itemDescription"][Math.min(currentShopItem * 2, dialogue["itemDescription"].length - 1)];
-                document.querySelectorAll("#description span")[1].textContent = dialogue["itemDescription"][Math.min(currentShopItem * 2 + 1, dialogue["itemDescription"].length - 1)];
+                arrayOfItems[currentShopIndex].style.transform = `translate${MOBILE ? "X" : "Y"}(0)`;
+                arrayOfItems[currentShopIndex].style.filter = "opacity(1)";
+                arrayOfItems[currentShopIndex].style.visibility = "visible";
+                const validIndex = Math.min(currentShopIndex, dialogue["itemHeader"].length - 1);
+                descriptionParts[0].textContent = dialogue["itemHeader"][validIndex];
+                descriptionParts[1].textContent = dialogue["itemDescription"][validIndex];
             });
         });
     } else {
+        // This triggers if we're just bantering.
         if (isMenuShowing) {
             toggleMenu();
         }
@@ -294,10 +307,7 @@ async function aliEvent() {
     }
     return;
 }
-async function shopClickEvent() {
-
-}
-async function displayText(text, speed = 1, location = "text", playSound = true) {
+async function displayText({text, speed = 1, location = "text", playSound = true}) {
     if (!isTyping) {
         const textBox = document.getElementById(location);
         textBox.innerHTML = "";
@@ -347,7 +357,7 @@ async function displayText(text, speed = 1, location = "text", playSound = true)
  * @param {boolean} playSound - Indicates whether or not this should play a sound.
  * @returns Null, just gives more control over when to continue with certain actions in cutscenes.
  */
-async function newText({ dialogueName, speed, location, playSound, starting, index }) {
+async function newText({ dialogueName, speed, location, playSound, starting: startingIndex, index }) {
     let openedDialogue = openDialogues.get(dialogueName);
     if (openedDialogue) {
         openedDialogue.clicks++;
@@ -358,8 +368,14 @@ async function newText({ dialogueName, speed, location, playSound, starting, ind
     const numberOfDialogues = dialogue[dialogueName].length;
     const currentIndex = Math.min(numberOfDialogues - 1, openedDialogue.clicks);
     // If an index is selected, then we use the index. Else, we clamp it.
-    const textToDisplay = dialogue[dialogueName][index ?? (starting ? Math.floor(Math.random() * (numberOfDialogues - 1)) + starting : currentIndex)];
-    await displayText(textToDisplay, speed, location, playSound);
+    const validIndex = index ?? (startingIndex ? (Math.floor(Math.random() * (numberOfDialogues - startingIndex)) + startingIndex) : currentIndex);
+    const textToDisplay = dialogue[dialogueName][validIndex];
+    await displayText({
+        text: textToDisplay, 
+        speed: speed, 
+        location: location, 
+        playSound: playSound
+    });
     return;
 }
 function createButton(id, top, left, width, height, functionName, div) {
@@ -374,22 +390,32 @@ function createButton(id, top, left, width, height, functionName, div) {
     document.getElementById(div).appendChild(button);
     button.addEventListener("pointerdown", functionName);
 }
-// Sets a screen so that I can use this between any two screens.
-async function setScreen(screenNumber, transitionTime, fadeOut, fadeIn) {
-    fadeOut = fadeOut ?? `${transitionTime / 1000}s`;
-    fadeIn = fadeIn ?? fadeOut;
-    const currentScreen = document.getElementById(`screen${currentScreenNumber}`);
-    const nextScreen = document.getElementById(`screen${screenNumber}`);
-    currentScreen.style.setProperty("--transition-time", fadeOut);
-    nextScreen.style.setProperty("--transition-time", fadeIn);
+/**
+ * This changes the screen from the current one to the next one. It has a couple of optional
+ * parameters, such as the fade out, fade in, and between screen time.
+ * @param {number} nextScreenIndex - The index of the next screen.
+ * @param {number} betweenScreenTime - The time between screens (black screen) in seconds.
+ * @param {number} fadeOut - The fade out time in seconds.
+ * @param {number} fadeIn - The fade in time in seconds.
+ */
+async function setScreen({
+    nextScreenIndex,
+    betweenScreenTime = 2000,
+    fadeOut = betweenScreenTime,
+    fadeIn = fadeOut
+}) {
+    const currentScreen = document.getElementById(`screen${currentScreenIndex}`);
+    const nextScreen = document.getElementById(`screen${nextScreenIndex}`);
+    currentScreen.style.setProperty("--transition-time", `${fadeOut / 1000}s`);
+    nextScreen.style.setProperty("--transition-time", `${fadeIn / 1000}s`);
     currentScreen.style.opacity = 0;
-    await sleep(transitionTime);
+    await sleep(betweenScreenTime);
     currentScreen.style.transform = "";
     currentScreen.style.display = "none";
     nextScreen.style.display = "flex";
     nextScreen.offsetHeight;
     nextScreen.style.opacity = 1;
-    currentScreenNumber = screenNumber;
+    currentScreenIndex = nextScreenIndex;
 }
 function toggleMenu() {
     const shopTabLabel = document.getElementById("shopTabLabel");
@@ -397,12 +423,12 @@ function toggleMenu() {
         if (!MOBILE) {
             shopContainer.style.right = "100px";
             shopTabLabel.textContent = "CLOSE SHOP";
-            document.getElementById("counter").style.transform = "translateX(-500px)";
+            counter.style.transform = "translateX(-500px)";
         } else {
             shopContainer.style.bottom = "0";
             shopTabLabel.textContent = "CLOSE SHOP";
-            document.getElementById("counter").style.transform = "translateY(-100px)";
-            document.getElementById("textDiv").style.transform = "translateY(-100px)";
+            counter.style.transform = "translateY(-100px)";
+            dialogueBox.style.transform = "translateY(-100px)";
             document.body.style.backgroundPositionY = "-190px";
         }
         isMenuShowing = true;
@@ -410,12 +436,12 @@ function toggleMenu() {
         if (!MOBILE) {
             shopContainer.style.right = "-900px";
             shopTabLabel.textContent = "OPEN SHOP";
-            document.getElementById("counter").style.transform = "translateX(0)";
+            counter.style.transform = "translateX(0)";
         } else {
             shopContainer.style.bottom = "-352px";
             shopTabLabel.textContent = "OPEN SHOP";
-            document.getElementById("counter").style.transform = "translateY(0)";
-            document.getElementById("textDiv").style.transform = "translateY(0)";
+            counter.style.transform = "translateY(0)";
+            dialogueBox.style.transform = "translateY(0)";
             document.body.style.backgroundPositionY = "0";
         }
         isMenuShowing = false;
